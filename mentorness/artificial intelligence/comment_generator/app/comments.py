2ec4a075788d35f9e54dff70bf041310b4a91337
Harshit-code-tech <harshitghosh6@gmail.com>
2024-07-20 09:02:37 +0530

    error

--- mentorness/artificial intelligence/comment_generator/app/comments.py
+++ mentorness/artificial intelligence/comment_generator/app/comments.py
@@ -1,5 +1,21 @@
 import random
 from .analysis import analyze_sentiment
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+from langdetect import detect
+
+def summarize_text(text, sentence_count=1):
+    language = detect(text)
+    parser = PlaintextParser.from_string(text, Tokenizer(language))
+    stemmer = Stemmer(language)
+    summarizer = LsaSummarizer(stemmer)
+    summarizer.stop_words = get_stop_words(language)
+
+    summary = summarizer(parser.document, sentence_count)
+    return " ".join(str(sentence) for sentence in summary)
 
 def generate_friendly_comment(content):
     friendly_templates = [
@@ -7,7 +23,7 @@ def generate_friendly_comment(content):
         "I really enjoyed this! {snippet}... Fantastic work!",
         "This is so well-written! {snippet}... Excellent job!"
     ]
-    snippet = content[:50]
+    snippet = summarize_text(content)
     return random.choice(friendly_templates).format(snippet=snippet)
 
 def generate_funny_comment(content):
@@ -16,7 +32,7 @@ def generate_funny_comment(content):
         "This cracked me up! {snippet}... So funny!",
         "I couldn't stop laughing at {snippet}... Great sense of humor!"
     ]
-    snippet = content[:50]
+    snippet = summarize_text(content)
     return random.choice(funny_templates).format(snippet=snippet)
 
 def generate_congratulating_comment(content):
@@ -25,7 +41,7 @@ def generate_congratulating_comment(content):
         "Well done! {snippet}... Keep up the great work!",
         "Bravo! {snippet}... Impressive achievement!"
     ]
-    snippet = content[:50]
+    snippet = summarize_text(content)
     return random.choice(congratulating_templates).format(snippet=snippet)
 
 def generate_questioning_comment(content):
@@ -34,7 +50,7 @@ def generate_questioning_comment(content):
         "Could you explain more about {snippet}...?",
         "I'd love to know more about {snippet}... Could you clarify?"
     ]
-    snippet = content[:50]
+    snippet = summarize_text(content)
     return random.choice(questioning_templates).format(snippet=snippet)
 
 def generate_disagreement_comment(content):
@@ -43,7 +59,7 @@ def generate_disagreement_comment(content):
         "Interesting perspective, but I see it differently: {snippet}...",
         "I respect your opinion, but {snippet}... doesn't resonate with me."
     ]
-    snippet = content[:50]
+    snippet = summarize_text(content)
     return random.choice(disagreement_templates).format(snippet=snippet)
 
 def generate_comments(content):
