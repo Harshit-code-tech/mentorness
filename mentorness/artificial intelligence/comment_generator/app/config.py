e4aaef92ae3567844883b96a4ed11ff71218007d
Harshit-code-tech <harshitghosh6@gmail.com>
2024-07-20 17:04:06 +0530

    Update config.py

--- mentorness/artificial intelligence/comment_generator/app/config.py
+++ mentorness/artificial intelligence/comment_generator/app/config.py
@@ -1,6 +1,17 @@
 # config.py
 class Config:
-    # Configuration settings can go here
     DEBUG = True
     SECRET_KEY = 'your_secret_key'
+    DATABASE_URI = 'sqlite:///your_database.db'
+    # Add any other settings you need
+class TestingConfig:
+    DEBUG = True
+    TESTING = True
+
+    SECRET = 'your_secret'
+    DATABASE_URI = 'sqlite:///your_database.db'
+    # Add any other settings you need
+    KEY = 'your_key'
+
+
     # Add any other settings you need
